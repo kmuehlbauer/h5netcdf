58ce042bfa02bda8d86eb6a4ab6e502c45cc4efb
kmuehlbauer <kmuehlbauer@users.noreply.github.com>
2022-03-16 10:56:35 +0000

    deploy: 2ffeb9e0b609b332e5a08d45755366d8c8097cae

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.15.0.dev1+g7277d00',
+    VERSION: '0.15.0.dev1+g2ffeb9e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
