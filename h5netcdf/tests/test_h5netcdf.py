14d0faf50fb26ce09ea453dc6870c465783bf91e
Kai Mühlbauer <kmuehlbauer@wradlib.org>
2024-01-20 20:50:27 +0100

    lint
    Signed-off-by: Kai Mühlbauer <kmuehlbauer@wradlib.org>

--- h5netcdf/tests/test_h5netcdf.py
+++ h5netcdf/tests/test_h5netcdf.py
@@ -2507,20 +2507,19 @@ def test_nc_complex_compatibility(tmp_local_or_remote_netcdf, netcdf_write_modul
 )
 def test_complex_type_creation_errors(tmp_local_netcdf):
     complex_array = np.array([0 + 0j, 1 + 0j, 0 + 1j, 1 + 1j, 0.25 + 0.75j])
-    kwargs = {}
 
-    with legacyapi.Dataset(tmp_local_netcdf, "w", **kwargs) as ds:
+    with legacyapi.Dataset(tmp_local_netcdf, "w") as ds:
         ds.createDimension("x", size=len(complex_array))
         with pytest.raises(TypeError, match="data type 'c4' not understood"):
-            var = ds.createVariable("data", "c4", ("x",))
+            ds.createVariable("data", "c4", ("x",))
 
-    with legacyapi.Dataset(tmp_local_netcdf, "w", **kwargs) as ds:
+    with legacyapi.Dataset(tmp_local_netcdf, "w") as ds:
         ds.createDimension("x", size=len(complex_array))
         with pytest.raises(
             TypeError,
             match="Currently only 'complex64' and 'complex128' dtypes are allowed.",
         ):
-            var = ds.createVariable("data", "c32", ("x",))
+            ds.createVariable("data", "c32", ("x",))
 
 
 def test_hsds(hsds_up):
